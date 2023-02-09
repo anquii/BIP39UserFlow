7e9718246ab014941840fdc15c0d536a07db1c0e
anquii <50277099+anquii@users.noreply.github.com>
2023-02-09 20:27:13 +0000

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "BIP39UserFlow",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS("16.1")
+    ],
+    products: [
+        .library(
+            name: "BIP39UserFlow",
+            targets: ["BIP39UserFlow"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/anquii/BIP39.git",
+            .upToNextMajor(from: "2.0.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "BIP39UserFlow",
+            dependencies: ["BIP39"]
+        ),
+        .testTarget(
+            name: "BIP39UserFlowTests",
+            dependencies: ["BIP39UserFlow"]
+        )
+    ]
+)
